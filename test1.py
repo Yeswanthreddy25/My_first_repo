1298526eeeb3d663fb15b201ec429dd55631a698
Yeswanth <yaswanthreddyc25@gmail.com>
2025-12-11 22:19:52 +0530

    updated test1

--- test1.py
+++ test1.py
@@ -1,1 +1,14 @@
-print("Hello World")
+name = "Yeswanth"
+print(name)
+#tupple
+tupple = (1,2,3,4,5)
+print(tupple)
+#list
+list = [1,2,3,4,5]
+print(list)
+#dictionary
+dictionary = {"name": "Yeswanth", "age": 20}
+print(dictionary)
+#set
+set = {1,2,3,4,5}
+print(set)
